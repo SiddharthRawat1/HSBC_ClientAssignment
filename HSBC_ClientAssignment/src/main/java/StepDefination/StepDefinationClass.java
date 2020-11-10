85b1216787484e79456cf27d0d00fb6b1ff36e2c
SiddharthRawat1 <73878166+SiddharthRawat1@users.noreply.github.com>
2020-11-10 14:53:37 +0530

    Update StepDefinationClass.java

--- HSBC_ClientAssignment/src/main/java/StepDefination/StepDefinationClass.java
+++ HSBC_ClientAssignment/src/main/java/StepDefination/StepDefinationClass.java
@@ -33,5 +33,32 @@ public class StepDefinationClass {
 		 System.out.println("Status Code of Response is :" + res.getStatusCode());
 
 	}
+@Then("Verify the success status code of the response is success")
+	public void validateAPIStatus() {
+		
+		//Applying Assert to validate the status code
+		 Assert.assertEquals(200, res.getStatusCode());
+	    
+		 //Printing the status code
+		 System.out.println("Status Code of Response is :" + res.getStatusCode());
+		 
+
+	}
+
+	@Then("validate response for negative response")
+	public void validate_response_for_negative_response() {
+		 Assert.assertEquals(400, res.getStatusCode());
+		 RestAssured.when().get("https://api.ratesapi.io/api/lates").then().log().all();
+		
+	}
+	
+	@Then("validate future positive response received")
+	public void validate_future_positive_response_received() {
+		 Assert.assertEquals(200, res.getStatusCode());
+		   Assert.assertEquals( "application/json",res.getContentType());
+		RestAssured.given().when().get("https://api.ratesapi.io/api/2022-01-12").then().body("date", equalTo("2020-11-06"));
+		// update above line everyday to passs this test or you can use LocalDate from java 
+	
+	}
 
 }
